976d1c49a5c53305856da4787544e26470c63de3
Samuel Solomon <mrsamuelsolomon@gmail.com>
2022-10-02 08:16:28 +0100

    creating and dispatching events

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +12,11 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    // Now any time a new Chirp is created, the ChirpCreated event will be dispatched.
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user()
